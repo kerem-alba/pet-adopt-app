f58f79a201864fa0ab860fad3143d5f9d0f7fa68
kerem-alba <c.kerem.albayrak@gmail.com>
2024-09-14 16:35:08 +0300

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 40,
+        }}
+      >
+        Welcome
+      </Text>
+    </View>
+  );
+}
